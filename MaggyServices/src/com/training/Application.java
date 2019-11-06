e51530e636ede395ba6fc761db35fda4c0581433
mounika <mounika.6298@gmail.com>
2019-11-06 15:18:57 +0530

    Third Request

--- MaggyServices/src/com/training/Application.java
+++ MaggyServices/src/com/training/Application.java
@@ -6,7 +6,7 @@ public class Application {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Hi Maggy,WELCOME TO Swiggy");
+		System.out.println("Hello Maggy,WELCOME TO Swiggy");
 		System.out.println("Developed by last bench");
 	}
 }
