b35ccb19f2bad2963ca87ae2ed127b1e740270d8
Nameless <'nameless.blunder19@gmail.com'>
2019-11-06 15:20:02 +0530

    Added Printer class

---
+++ MaggyServices/src/com/training/PrintBill.java
@@ -0,0 +1,34 @@
+package com.training;
+
+public class PrintBill {
+
+	private String service;
+	private long amt;
+	public PrintBill(String service, long amt) {
+		super();
+		this.service = service;
+		this.amt = amt;
+	}
+	public PrintBill() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getService() {
+		return service;
+	}
+	public void setService(String service) {
+		this.service = service;
+	}
+	public long getAmt() {
+		return amt;
+	}
+	public void setAmt(long amt) {
+		this.amt = amt;
+	}
+	@Override
+	public String toString() {
+		return "PrintBill [service=" + service + ", amt=" + amt + "]";
+	}
+	
+	
+}
